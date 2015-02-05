38844504a9bdbe5c820f88885868fcc4c410f7b1
saikavya <saikavya.dukkipati@mavs.uta.edu>
2015-02-05 16:20:54 -0600

    Amazon EBS,Twilio
    Developed a simple application using GOOGLE MAPS and TWILIO and deployed
    it on Amazon EBS and also on Amazon Cloud-front.

---
+++ mummyplease.php
@@ -0,0 +1,7 @@
+<?php
+       header ('content-type: text/xml');
+       echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+?>
+<Response>
+    <Sms>For GeoLocation please open the link http://omega.uta.edu/~sxd0778/geo.html </Sms>
+</Response>
